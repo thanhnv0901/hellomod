7b8daff5c2bf8438485d7edb809d732d3a83d5b8
Nguyen Thanh <nguyenthanh@192.168.0.106>
2020-06-20 14:24:40 +0700

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/thanhnv0901/hellomod/v3
+module github.com/thanhnv0901/hellomod/v4
 
 go 1.14
--- hellomod.go
+++ hellomod.go
@@ -7,6 +7,6 @@ import (
 //SayHello function
 func SayHello(name string) {
 	fmt.Println("Thanh's module")
-	fmt.Println("Version 3 without branch v3")
-	fmt.Println("Hello World! version: 3.0.0")
+	fmt.Println("Version 4 without branch v4")
+	fmt.Println("Hello World! version: 4.0.0")
 }
